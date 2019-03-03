22bb864c4badb377bc08e0386519a0990cf8bcbe
Myhr86 <stein_myhr@hotmail.com>
2019-03-03 17:52:38 +0100

    Changed file structure, added image and some styling

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 var foods = ["Lasagne","Taco","Pizza","Gryterett","Spagetti","Fiskegrateng","Pølse og mos","Sodd","Buritos",
 "Pannekaker","Hamburger","Wok","Pølse og brød","Laksepakker","Fiskepinner","Ørretfilet","Fiskeboller i hvit saus",
-"Tomatsuppe","Ostepanetter","Lapskaus"];
+"Tomatsuppe","Ostepanetter"];
 var days = ["Monday: ", "Tuesday: ", "Wednesday: ", "Thursday: ", "Friday: ", "Saturday: ", "Sunday: "]
 
 
@@ -13,7 +13,15 @@ for (let i = 0; i < foods.length; i++) {
 
 function newDinner() {
 var randomDinner = (Math.floor(Math.random()*foods.length));
+var randomDinner2 = (Math.floor(Math.random()*foods.length));
+var randomDinner3 = (Math.floor(Math.random()*foods.length));
+var randomDinner4 = (Math.floor(Math.random()*foods.length));
+var randomDinner5 = (Math.floor(Math.random()*foods.length));
+var randomDinner6 = (Math.floor(Math.random()*foods.length));
+var randomDinner7 = (Math.floor(Math.random()*foods.length));
+
 var randomNumber = (Math.floor(Math.random() * 1) + 1);
+console.log(randomNumber);
 
 document.getElementById("foods").innerHTML = foods[randomDinner] + "<br>" + foods[randomDinner + randomNumber] + "<br>" + foods[randomDinner + 2] + "<br>" + foods[randomDinner + 3] +
 "<br>" + foods[randomDinner - 3] + "<br>" + foods[randomDinner - 2] + "<br>" + foods[randomDinner -1];
